8321b63fbffcab9a3149b392851e2bdade609fcd
tajud99n <giwa208@gmail.com>
2019-06-29 19:43:57 +0100

    authentication middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,19 @@
+// Packages
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token.');
+    }
+};
+
+module.exports = { authenticate };
