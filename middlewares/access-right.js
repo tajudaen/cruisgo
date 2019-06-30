a2e2eec0fa99aacabddb1a7e304bcbc9dbd29077
tajud99n <giwa208@gmail.com>
2019-06-30 15:12:01 +0100

    access authorization for creating, reading, updating and deleting

--- middlewares/access-right.js
+++ middlewares/access-right.js
@@ -8,3 +8,33 @@ exports.createAccess = (req, res, next) => {
     next();
     
 }
+exports.readAccess = (req, res, next) => {
+    const permissions = req.user.permissions;
+
+    if (!permissions.includes("read")) {
+        return res.status(403).send('Access denied');
+    }
+    
+    next();
+    
+}
+exports.updateAccess = (req, res, next) => {
+    const permissions = req.user.permissions;
+
+    if (!permissions.includes("update")) {
+        return res.status(403).send('Access denied');
+    }
+    
+    next();
+    
+}
+exports.deleteAccess = (req, res, next) => {
+    const permissions = req.user.permissions;
+
+    if (!permissions.includes("delete")) {
+        return res.status(403).send('Access denied');
+    }
+    
+    next();
+    
+}
--- test/units/middleware/access-right.test.js
+++ test/units/middleware/access-right.test.js
@@ -2,15 +2,14 @@
 const expect = require('expect');
 const sinon = require('sinon');
 
-const { User } = require('../../../models/user');
-const { createAccess } = require('../../../middlewares/access-right');
+const { createAccess, readAccess, updateAccess, deleteAccess } = require('../../../middlewares/access-right');
 
 describe('access middleware', () => {
     it('should return 403 if user doesnt have necessary permissions to create', () => {
 
         const req = {
             user: {
-                permissions: []
+                permissions: ["read"]
             }
         };
 
@@ -29,5 +28,180 @@ describe('access middleware', () => {
 
         expect(res.status.calledWith(403)).toBeTruthy();
         expect(res.send.calledWith('Access denied')).toBeTruthy();
+    });
+
+    it('should return not a 403 if user have necessary permissions to create', () => {
+
+        const req = {
+            user: {
+                permissions: ["create"]
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        createAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeFalsy();
+        expect(res.send.calledWith('Access denied')).toBeFalsy();
+    });
+
+    it('should return 403 if user doesnt have necessary permissions to read', () => {
+
+        const req = {
+            user: {
+                permissions: []
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        readAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeTruthy();
+        expect(res.send.calledWith('Access denied')).toBeTruthy();
+    });
+
+    it('should return not a 403 if user have necessary permissions to read', () => {
+
+        const req = {
+            user: {
+                permissions: ["read"]
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        readAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeFalsy();
+        expect(res.send.calledWith('Access denied')).toBeFalsy();
+    });
+
+    it('should return 403 if user doesnt have necessary permissions to update', () => {
+
+        const req = {
+            user: {
+                permissions: []
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        updateAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeTruthy();
+        expect(res.send.calledWith('Access denied')).toBeTruthy();
+    });
+
+    it('should return not a 403 if user have necessary permissions to update', () => {
+
+        const req = {
+            user: {
+                permissions: ["update"]
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        updateAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeFalsy();
+        expect(res.send.calledWith('Access denied')).toBeFalsy();
+    });
+
+    it('should return 403 if user doesnt have necessary permissions to delete', () => {
+
+        const req = {
+            user: {
+                permissions: ["read"]
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        deleteAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeTruthy();
+        expect(res.send.calledWith('Access denied')).toBeTruthy();
+    });
+
+    it('should return not a 403 if user have necessary permissions to delete', () => {
+
+        const req = {
+            user: {
+                permissions: ["delete"]
+            }
+        };
+
+        const mockResponse = () => {
+            const res = {};
+            res.status = sinon.stub().returns(res);
+            res.send = sinon.stub().returns(res);
+            return res;
+        };
+
+        const res = mockResponse();
+
+        const next = sinon.stub();
+
+        deleteAccess(req, res, next);
+
+        expect(res.status.calledWith(403)).toBeFalsy();
+        expect(res.send.calledWith('Access denied')).toBeFalsy();
     });
 });
